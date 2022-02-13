1631d0c653d758b70346b8f98bca93032bd8cea1
wangfong232 <86735806+wangfong232@users.noreply.github.com>
2022-02-13 16:16:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'crush à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim mình lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hjhj',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Mình thích cậu <33',
+    btnNo: 'Không, tụi mình chỉ là bạn :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích mình <3',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'mình thích cậu, cậu có thích mình khum 😘😘',
+    messDesc: 'iu cậu quá :3.',
+    btnAccept: 'yêu nhau luôn đi <3',
+    messLink: 'http://fb.com/chancewangfong'
+}
